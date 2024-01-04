aca21323cda7c48513b3a1c530084543a8266f88
genai-jerry <jerry@genaipeople.com>
2024-01-04 12:02:37 +0530

    Fixing issues with loading QR Code

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -28,38 +28,39 @@ def __generate_qr_code(browser, app_home, host_number):
             print('Waiting for qr_code')
             qr_div = browser.find_element_by_xpath('//div[@class="_19vUU"]')
             print('Page is ready!')
-            start_time = time.time()  # Record the start time
+            page_start_time = time.time()  # Record the start time
             timeout = 30  # Timeout in seconds
-            while True:
-                try:
-                    print('Waiting for data-ref')
-                    # Get the canvas as a PNG base64 string
-                    qr_div = browser.find_element_by_xpath('//div[@class="_19vUU"]')
-                    data_ref = qr_div.get_attribute("data-ref")
-                    if data_ref != None:
-                        print(f'Got data {data_ref}')
-                        qr.add_data(data_ref)
-                        print('Fitting qr')
-                        qr.make(fit=True)
-                        print('Making Image')
-                        # Create an image of the QR code
-                        img = qr.make_image(fill_color="black", back_color="white")
-
-                        # Save the QR code image to a file
-                        image_path = os.path.join(app_home, image_save_path)
-                        print(f'Storing QR code at {image_path}')
-                        qr_file_path = f"{image_path}/whatsapp_web_qr_{host_number}.png"
-                        print(f'Saving image to {qr_file_path}')
-                        if os.path.exists(qr_file_path):
-                            os.remove(qr_file_path)
-                        img.save(qr_file_path)
-                        return True
-                except Exception as e:
-                    print(f'{e}')
-                if time.time() - start_time > timeout:
-                    raise RuntimeError('Unable to load QR Code')
-                time.sleep(1)
         except:
             if time.time() - start_time > timeout:
                 print('Loading took too much time!')
                 raise RuntimeError('Unable to load QR Code')
+        while True:
+            try:
+                print('Waiting for data-ref')
+                # Get the canvas as a PNG base64 string
+                qr_div = browser.find_element_by_xpath('//div[@class="_19vUU"]')
+                data_ref = qr_div.get_attribute("data-ref")
+                if data_ref != None:
+                    print(f'Got data {data_ref}')
+                    qr.add_data(data_ref)
+                    print('Fitting qr')
+                    qr.make(fit=True)
+                    print('Making Image')
+                    # Create an image of the QR code
+                    img = qr.make_image(fill_color="black", back_color="white")
+
+                    # Save the QR code image to a file
+                    image_path = os.path.join(app_home, image_save_path)
+                    print(f'Storing QR code at {image_path}')
+                    qr_file_path = f"{image_path}/whatsapp_web_qr_{host_number}.png"
+                    print(f'Saving image to {qr_file_path}')
+                    if os.path.exists(qr_file_path):
+                        os.remove(qr_file_path)
+                    img.save(qr_file_path)
+                    return True
+            except Exception as e:
+                print(f'{e}')
+            if time.time() - page_start_time > timeout:
+                raise RuntimeError('Unable to load QR Code')
+            time.sleep(1)
+        
