9bf3c7e6a554973a5c0912bb06283f714accf682
genai-jerry <jerry@genaipeople.com>
2024-01-04 11:34:13 +0530

    Updating driver config and selenium

--- process_manager/instance_manager.py
+++ process_manager/instance_manager.py
@@ -80,7 +80,7 @@ def create_instance(app_home, mobile_number):
     try:
         make_file_executable(driver_file_path)
         print('Loading chrome web driver')
-        browser = webdriver.Chrome(executable_path=driver_file_path, timeout=60,
+        browser = webdriver.Chrome(executable_path=driver_file_path,
                                    options=options, keep_alive=True) 
         print('Getting the whatsapp web')
         browser.get(whatsapp_web_url)  
