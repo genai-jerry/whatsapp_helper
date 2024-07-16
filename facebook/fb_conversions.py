704ef27a878c07e2810995f5471e9b7d0f79da90
genai-jerry <jerry@genaipeople.com>
2024-07-17 00:26:23 +0530

    Fixed FB CAPI

--- facebook/fb_conversions.py
+++ facebook/fb_conversions.py
@@ -48,5 +48,11 @@ def fire_event(opportunity, event_name):
         "Account ID": ad_account
     }
     headers = {"Content-Type": "application/json"}
-
+    
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    if response.status_code == 200:
+        print("Successfully sent data to external API")
+    else:
+        print("Failed to send data to external API")
+        print(response.text)
 
