ace58706baf36415eaa0ae3649b5f0c965fda5ab
genai-jerry <jerry@genaipeople.com>
2024-11-14 19:51:02 +0530

    Fixes

--- static/js/call_setting_pagination.js
+++ static/js/call_setting_pagination.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.page-link').forEach(link => {
         link.addEventListener('click', function(e) {
             e.preventDefault();
-            e.stopPropagation();
             // Get the closest pipeline-list parent to identify the type
             const pipelineList = this.closest('.pipeline-list');
             const type = pipelineList ? pipelineList.id : '';
